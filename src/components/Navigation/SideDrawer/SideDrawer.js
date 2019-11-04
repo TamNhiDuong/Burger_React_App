47b0dd0f35a185ff181f8a0f92d7763fedc12112
TamNhiDuong <yanxinyee@gmail.com>
2019-11-04 21:13:25 +0200

    Posting to Firebase

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,12 +3,12 @@ import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
 import Backdrop from '../../UI/Backdrop/Backdrop';
-import Aux from '../../../hoc/Aux'; //To be able to add reused Backdrop to side-drawer
+import Aux from '../../../hoc/Aux/Aux'; //To be able to add reused Backdrop to side-drawer
 const sideDrawer = (props) => {
     //Adding attached classes to have animation of close-open SideDrawer, if only assigned css-class-SideDrawer
     //Then the SideDrawer always open
     //join'' to turn the Array to a single string 
-    //???Toggle?
+    
     let attachedClasses = [classes.SideDrawer, classes.Close];
     if(props.open) {
         attachedClasses = [classes.SideDrawer, classes.Open]; 
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
-import Aux from "../../hoc/Aux";
+import Aux from "../../hoc/Aux/Aux";
 import Burger from '../../components/Burger/Burger'; 
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'; 
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner'; 
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -22,7 +24,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false, 
     }
     updatePurchaseState(ingredients) {
         //const ingredients = {
@@ -45,8 +48,34 @@ class BurgerBuilder extends Component {
         this.setState({purchasing: false});
     };
     continuePurchaseHandler = () => {
-        alert("You are continuing!")
-    };
+        //Change loading to true as orderSummary is about to be shown
+        this.setState({loading: true})
+        //alert("You are continuing!")
+        //Sending data to Firebase, endpoint: /orders.json. In which 'orders' is random. 
+        //order is data object
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Yang Yee',
+                address: {
+                    street: 'Kilonrinne 10 B36',
+                    zipCode: '02610',
+                    country: 'Finland'
+                },
+                email: 'a1801032@myy.haaga-helia.fi'
+            },
+            deliveryMethod: 'Foodora'
+        }
+        axios.post('/orders.json', order)
+        .then( response => {
+            this.setState({loading: false, purchasing: false});
+        })
+        .catch(error =>{
+            this.setState({loading: false, purchasing: false});
+        });
+    }
+
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
         const updatedCount = oldCount + 1;
@@ -84,6 +113,16 @@ class BurgerBuilder extends Component {
         const disabledInfor = {
             ...this.state.ingredients
         };
+        //Show spinner or OrderSummary? False or true?
+        let orderSummary = <OrderSummary 
+        ingredients= {this.state.ingredients}
+        cancelPurchase={this.cancelPurchaseHandler}
+        continuePurchase={this.continuePurchaseHandler}
+        price={this.state.totalPrice}
+        />
+        if (this.state.loading) {
+            orderSummary = <Spinner/> ; 
+        }
         //check true/ false, if true then disable the button
         //{salad: true; meat: false,...}
         for (let key in disabledInfor) {
@@ -92,12 +131,7 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} cancelPurchase={this.cancelPurchaseHandler}>
-                    <OrderSummary 
-                    ingredients= {this.state.ingredients}
-                    cancelPurchase={this.cancelPurchaseHandler}
-                    continuePurchase={this.continuePurchaseHandler}
-                    price={this.state.totalPrice}
-                    />
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
